7171425a00434d263e96abca5262f49fefab4484
adrian viljoen <adrian.v8@icloud.com>
2023-01-10 12:17:48 +0200

    first commit for this project

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import "../styles/index.css";
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Drum machine</h1>
+    </div>
+  )
+}
+
+export default App
