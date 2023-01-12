e616e4a6d1edb5530f4dc0031b0fe2be3f3eac04
adrian viljoen <adrian.v8@icloud.com>
2023-01-12 11:26:51 +0200

    still building the layout

--- src/components/MPCScreen.jsx
+++ src/components/MPCScreen.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
+import "../styles/mpcscreen.css"
 
 function MPCScreen() {
   return (
-    <div className='screen-container'>
-        <div className='screen'>This is an Akai MPC....well it will be</div>
-    </div>
+      <div className='screen'>This is an Akai MPC....well it will be</div>
   )
 }
 
